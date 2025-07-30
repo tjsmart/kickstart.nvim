17e8bcb132a9277a300747e95b0fdd0d7ddecf81
Tyler Smart <tjsmart@ucsc.edu>
2025-07-30 15:27:30 -0700

    Add means for repo-specific settings

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,4 @@
 -- This file is loaded by init.lua, it will scan and load modules which are dropped in this directory.
---
 local ok, scandir = pcall(require, 'plenary.scandir')
 if not ok then
   vim.notify('custom/init.lua requires plenary.nvim', vim.log.levels.ERROR)
@@ -17,5 +16,20 @@ for _, mod_path in ipairs(modules) do
   ok, err = pcall(require, 'custom.' .. mod_name)
   if not ok then
     vim.notify('Error loading custom.' .. mod_name .. ': ' .. err, vim.log.levels.ERROR)
+  end
+end
+
+-- In addition, check for a .nvim.lua file in the current working directory.
+-- If it exists, it will load it.
+local cwd_nvim = vim.fn.getcwd() .. '/.nvim.lua'
+if vim.fn.filereadable(cwd_nvim) == 1 then
+  local cwd_nvim_config = loadfile(cwd_nvim)
+  if cwd_nvim_config then
+    ok, err = pcall(cwd_nvim_config)
+    if not ok then
+      vim.notify('Error loading .nvim.lua: ' .. err, vim.log.levels.ERROR)
+    end
+  else
+    vim.notify('Could not load .nvim.lua', vim.log.levels.ERROR)
   end
 end

4477ae713337a5e03a0ec52fffe97c7ffeb912dc
Tyler Smart <tsmart29@hotmail.com>
2025-11-18 18:16:59 -0800

    forward window commands in terminal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,20 +24,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', { desc = 'Move focus from terminal window to the left window' })
-vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', { desc = 'Move focus from terminal window to the right window' })
-vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', { desc = 'Move focus from terminal window to the lower window' })
-vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', { desc = 'Move focus from terminal window to the upper window' })
-
 vim.keymap.set('n', '<C-Down>', '<C-w>-', { desc = 'Descrease window height' })
 vim.keymap.set('n', '<C-Up>', '<C-w>+', { desc = 'Increase window height' })
 vim.keymap.set('n', '<C-Left>', '<C-w><', { desc = 'Decrease window width' })
 vim.keymap.set('n', '<C-Right>', '<C-w>>', { desc = 'Increase window width' })
 
-vim.keymap.set('t', '<C-Down>', '<C-\\><C-n><C-w>-', { desc = 'Descrease terminal window height' })
-vim.keymap.set('t', '<C-Up>', '<C-\\><C-n><C-w>+', { desc = 'Increase terminal window height' })
-vim.keymap.set('t', '<C-Left>', '<C-\\><C-n><C-w><', { desc = 'Decrease terminal window width' })
-vim.keymap.set('t', '<C-Right>', '<C-\\><C-n><C-w>>', { desc = 'Increase terminal window width' })
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], { noremap = true, desc = 'Forward window commands in terminal mode' })
 
 vim.keymap.set('n', '<leader><leader>c', function()
   local cb = vim.opt.clipboard:get()
